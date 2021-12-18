d2d8e0c8a831e01a3538aa5e9fc2fd261a67d7c0
denghui <88145482@qq.com>
2021-12-18 18:25:18 +0800

    up

--- smpte_428-7xml/__init__.py
+++ smpte_428-7xml/__init__.py
@@ -1,31 +1,49 @@
 import xml.etree.ElementTree as ET
 
+
 class Canvars():
     def __init__(self):
-        self.xml_version = ET.Element("xml", {"versioon": "1.0", "encoding": "UTF-8"})
+        self._xml_version = ET.Element("xml", {"version": "1.0", "encoding": "UTF-8"})
         self.SubtitleReel = ET.Element("SubtitleReel", {"xmlns": "http://www.smpte-ra.org/schemas/428-7/2010/DCST",
-                                                   "xmlns:xs": "http://www.w3.org/2001/schema"})
-
-    def
+                                                        "xmlns:xs": "http://www.w3.org/2001/schema"})
+        self.double_subtitle = False
+        self.double_font = False
+        self._SubtitleReel_main()
+        self._subtitle_attrib = {"SpotNumber": "0", "TimeIn": "00:00:00:00", "TimeOut": "00:00:00:00","FadeUpTime": "00:00:00:00", "FadeDownTime": "00:00:00:00"}
+        self.text_attrib = {"Hposition": "0.0", "Halign": "center", "Valign": "bottom", "Vposition": "0.0",
+                            "Direction": "ltr"}
+        self.effect_attrib = {"ID": "SimHei", "Color": "FFFFFFFF", "Weight": "normal", "Size": "36", "Effect": "shadow",
+                              "EffectColor": "FF000000", "AspectAdjust": "1.00"}
         self.SubtitleList = ET.SubElement(self.SubtitleReel, "SubtitleList")
+
+    def __str__(self):
+        return "{}".format(ET.dump(self.SubtitleReel))
+
+    def _SubtitleReel_main(self):
+        "为SubtitleReel下增加节点"
+        self.ID = ET.SubElement(self.SubtitleReel, "ID")
+        self.ContentTitleText = ET.SubElement(self.SubtitleReel, "ContentTitleText")
+        self.AnnotationText = ET.SubElement(self.SubtitleReel, "AnnotationText")
+        self.IssueDate = ET.SubElement(self.SubtitleReel, "IssueDate")
+        self.ReelNumber = ET.SubElement(self.SubtitleReel, "ReelNumber")
+        self.Language = ET.SubElement(self.SubtitleReel, "Language")
+        self.EditRate = ET.SubElement(self.SubtitleReel, "EditRate")
+        self.TimeCodeRate = ET.SubElement(self.SubtitleReel, "TimeCodeRate")
+        self.StartTime = ET.SubElement(self.SubtitleReel, "StartTime")
+        self.LoadFont = ET.SubElement(self.SubtitleReel, "LoadFont", {"ID": ""})
+
+    def subtitle_info(self,textinfo:list):
+        self.subtitle = ET.SubElement(self.SubtitleList, "subtitle", self._subtitle_attrib)
+        self.subtitle.attrib[]
+        if self.double_subtitle:
+            self.Text = ET.SubElement(self.subtitle,"Text",self.text_attrib)
+            if self.double_font:
+                self.subtitle2 = ET.SubElement(self.subtitle, "subtitle", self._subtitle_attrib)
+                self.Text2 = ET.SubElement(self.subtitle2, "Text", self.text_attrib)
+        else:
+            for text in textinfo[-1]:
+                ET.SubElement(self.subtitle,"Text",)
 
 
 if __name__ == '__main__':
-    xml_version = ET.Element("xml", {"versioon": "1.0", "encoding": "UTF-8"})
-    SubtitleReel = ET.Element("SubtitleReel", {"xmlns": "http://www.smpte-ra.org/schemas/428-7/2010/DCST",
-                                                   "xmlns:xs": "http://www.w3.org/2001/schema"})
-    ID = ET.SubElement(SubtitleReel, "ID")
-    ContentTitleText = ET.SubElement(SubtitleReel, "ContentTitleText")
-    AnnotationText = ET.SubElement(SubtitleReel, "AnnotationText")
-    IssueDate = ET.SubElement(SubtitleReel, "IssueDate")
-    ReelNumber = ET.SubElement(SubtitleReel, "ReelNumber")
-    Language = ET.SubElement(SubtitleReel, "Language")
-    EditRate = ET.SubElement(SubtitleReel, "EditRate")
-    TimeCodeRate = ET.SubElement(SubtitleReel, "TimeCodeRate")
-    StartTime = ET.SubElement(SubtitleReel, "StartTime")
-    LoadFont = ET.SubElement(SubtitleReel, "LoadFont",{"ID":""})
-    SubtitleList = ET.SubElement(SubtitleReel, "SubtitleList")
-
-
-
-    ET.dump(SubtitleReel)
+    print(Canvars())
--- srt_pase/__init__.py
+++ srt_pase/__init__.py
@@ -1,30 +1,26 @@
-
 import pysrt
 from smpte_timecode import *
-import pandas as pd
 
 s = SMPTE()
 
 
 def ms_trans(timecode: str, fps: [float, int]):
-
     timecode = timecode.replace(",", ":")
     H, M, S, MS = timecode.split(":")
     Timecode_S = 3600 * int(H) + 60 * int(M) + int(S)
-    Timecode_MS = (int(MS) / 1000) / (1/fps)
+    Timecode_MS = (int(MS) / 1000) / (1 / fps)
     s.gettc(fps)
     return s.gettc((Timecode_S * 24) + Timecode_MS)
 
 
+def srt_pase(srt_file):
+    data = pysrt.open(srt_file)
+    result = []
+    for x in data:
+        result.append([x.index, ms_trans(str(x.start), 24), ms_trans(str(x.end), 24), x.text.split("\n")])
+    return result
+
 
 if __name__ == '__main__':
-    data = pysrt.open("/Users/macintosh/PycharmProjects/Cinecanvas/resource/IMAX.srt")
-    csvfile = "/Users/macintosh/PycharmProjects/Cinecanvas/srt_pase/test.csv"
-
-    xy = []
-    for x in data:
-        # t = "{}--->{}---->{}---->{}".format(x.index, ms_trans(str(x.start), 24), ms_trans(str(x.end), 24), x.text)
-        xy.append([x.index, ms_trans(str(x.start), 24), ms_trans(str(x.end), 24), x.text])
-
-    final = pd.DataFrame(xy,columns=["index","start","end","sub"])
-    final.to_csv(csvfile,encoding="utf-8")
+    x = srt_pase("/Users/denghui/PycharmProjects/Cinecanvas/resource/IMAX.srt")
+    print(x)
